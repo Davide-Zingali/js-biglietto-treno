132d88cbb6492df94fb4c8c7aa04e58c20770e70
Davide <kalelc351k@gmail.com>
2020-10-20 20:33:15 +0200

    struttura base js completata

--- js/script.js
+++ js/script.js
@@ -1,27 +1,25 @@
 // calcolo del prezzo del biglietto del treno
 // Richiesta per utente: numero di chilometri che vuole percorrere
-var numeroKm = prompt("Dove vuoi andare? Dimmi i km");
+var numeroKm = prompt("Dove vuoi andare? Indicami quanti km percorrerai:");
 
 // Richiesta per utente: età del passeggero.
-var etaUtente = prompt("Qual'è la tua età");
-console.log(numeroKm, etaUtente);
+var etaUtente = prompt("Qual'è la tua età?");
 
-// Prezzo totale del viaggio, secondo queste regole:
-// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// prezzo del biglietto definito in base ai km (0.21 € al km)
 var prezzoKm = (0.21 * numeroKm);
-console.log(prezzoKm);
-// va applicato uno sconto del 20% per i minorenni
 
-// va applicato uno sconto del 40% per gli over 65
+// variabili sconti
+  var sconto20minorenni = ((20 * prezzoKm) / 100);
+  var sconto40Over65 = ((40 * prezzoKm) / 100);
 
+// condizioni in base all'età
+if (etaUtente < 18) {
+  var prezzoTotale = prezzoKm - sconto20minorenni;
+} else if (etaUtente > 65) {
+  var prezzoTotale = prezzoKm - sconto40Over65;
+} else {
+  var prezzoTotale = prezzoKm
+};
 
-
-
-
-
-
-
-// BONUS
-// do al mio aoutput un aspetto avanzato con html+css
-// NOTE
-// come detto: dare un output sensato, cioè con al max 2 decimali (cercare come si fa)
+// visualizzazione
+document.getElementById('risultato').innerHTML="Il prezzo del tuo biglietto per i km stabiliti è: " + prezzoTotale + "€";
